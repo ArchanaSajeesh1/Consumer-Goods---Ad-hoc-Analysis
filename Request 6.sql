fd4d301df64bdcdcc5f02f9d94ccdce1329e89d8
ArchanaSajeesh1 <151387987+ArchanaSajeesh1@users.noreply.github.com>
2023-11-19 06:54:55 +0530

    Add files via upload

---
+++ Request 6.sql
@@ -0,0 +1,13 @@
+/*Generate a report which contains the top 5 customers who received an average high pre_invoice_discount_pct  for the  fiscal  year 2021  and in the Indian  market. The final output contains these fields, 
+customer_code 
+customer 
+average_discount_percentage */
+
+SELECT c.customer_code,c.customer,ROUND(AVG(pre_invoice_discount_pct*100),2) as Average_discount_percentage
+FROM dim_customer c
+JOIN fact_pre_invoice_deductions i
+ON i.customer_code = c.customer_code
+WHERE market = "india" and fiscal_year = 2021
+group by customer,customer_code
+order by Average_discount_percentage DESC
+LIMIT 5;
